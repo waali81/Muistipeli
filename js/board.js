9257e0e4defe8267ffa6919158348539e53b69a4
Timo Waali <waali.timo@gmail.com>
2025-10-24 09:44:37 +0300

    kirjoitusvirhe

--- js/board.js
+++ js/board.js
@@ -9,7 +9,7 @@ let secondCard = null;
 let lockBoard = false;
 
 function shuffle(array) {
-    for (let i = array.lenght - 1; i > 0; i--) {
+    for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
